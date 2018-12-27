03a103fec8d3f812153193b36221ad68a5fa97c8
david_sun <sunao2002002@gmail.com>
2018-12-27 23:47:07 +0800

    58 length of last word

---
+++ 58_length_of_last_word.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int lengthOfLastWord(char* s) {
+    if (s ==NULL)
+        return 0;
+    int count = 0;
+    int length = strlen(s);
+    char *p = s + length -1;
+    int index = length-1;
+    for(; index>=0; index--){
+        if(s[index] != ' ')
+            break;
+    }
+    for(; index>=0; index--){
+        if(s[index] == ' ')
+            break;
+        count++;
+    }
+    return count;
+    
+}
+int main(int argc, char ** argv)
+{
+    printf("%d\n", lengthOfLastWord("Hello World"));
+}
